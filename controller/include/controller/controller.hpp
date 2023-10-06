32d3a553285c1630dc3da295a13a5f00bb5c77ad
Gowtham97 <gowthammurugesan1997@gmail.com>
2023-10-06 18:35:18 +0000

    added CONTROL typedef and other variables to control teleoperation

--- controller/include/controller/controller.hpp
+++ controller/include/controller/controller.hpp
@@ -11,9 +11,13 @@ class Controller {
         Controller(ros::NodeHandle nh);
         ~Controller();
     private:
+        typedef controller_msgs::Control::Request CONTROL;
         ros::Publisher cmd_publisher;
         ros::Subscriber cmd_subscriber;
         ros::ServiceServer control_service;
+        geometry_msgs::Twist current_cmd_vel;
+        bool teleop_enabled;
+        bool emergency_stop;
         bool control_cb(controller_msgs::Control::Request &req,
                 controller_msgs::ControlResponse &res);
         void cmd_vel_cb(const geometry_msgs::Twist::ConstPtr& cmd);
